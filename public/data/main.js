7cb95ab40a7f46537ef383f459c17c66e77dc1e7
Carlos Blanco <carlosblanco@me.com>
2019-11-28 20:58:39 +0100

    adding APL data & templates

---
+++ public/data/main.js
@@ -0,0 +1,52 @@
+{
+    "bodyTemplate6Data": {
+        "type": "object",
+        "objectId": "bt6Sample",
+        "backgroundImage": {
+            "contentDescription": null,
+            "smallSourceUrl": null,
+            "largeSourceUrl": null,
+            "sources": [
+                {
+                    "url": "https://mi-huerto-skill.firebaseapp.com/vegetables.jpg",
+                    "size": "small",
+                    "widthPixels": 0,
+                    "heightPixels": 0
+                },
+                {
+                    "url": "https://mi-huerto-skill.firebaseapp.com/vegetables.jpg",
+                    "size": "large",
+                    "widthPixels": 0,
+                    "heightPixels": 0
+                }
+            ]
+        },
+        "image": {
+            "contentDescription": null,
+            "smallSourceUrl": null,
+            "largeSourceUrl": null,
+            "sources": [
+                {
+                    "url": "https://mi-huerto-skill.firebaseapp.com/vegetables-250.jpg",
+                    "size": "small",
+                    "widthPixels": 0,
+                    "heightPixels": 0
+                },
+                {
+                    "url": "https://mi-huerto-skill.firebaseapp.com/vegetables-250.jpg",
+                    "size": "large",
+                    "widthPixels": 0,
+                    "heightPixels": 0
+                }
+            ]
+        },
+        "textContent": {
+            "primaryText": {
+                "type": "PlainText",
+                "text": "Bienvenidos a Mi Huerto"
+            }
+        },
+        "logoUrl": "https://mi-huerto-skill.firebaseapp.com/mihuerto-icon.png",
+        "hintText": "Prueba, \"Alexa, ¿Que puedo sembrar?\""
+    }
+}
